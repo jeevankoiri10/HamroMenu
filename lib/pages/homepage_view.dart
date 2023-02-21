ab9120a6bd34601ac6e289403438da8750c6bd41
jeevankoiri10 <jeevankoirima@gmail.com>
2023-02-22 01:18:50 +0545

    Home page images changed

--- lib/pages/homepage_view.dart
+++ lib/pages/homepage_view.dart
@@ -233,21 +233,75 @@ class _HomePageState extends State<HomePage> {
                           // scrollDirection: Axis.vertical,
                           child: Column(
                             children: [
-                              Image.network(
-                                "https://previews.123rf.com/images/winnond/winnond2008/winnond200800004/152889333-find-nearby-restaurants-that-open-for-food-delivery-or-search-location-of-the-restaurant-for-a-meal-.jpg",
-                                height: 200,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  'https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/09/CoverKathmanduRestoepb0310.jpg',
+                                  height: 200,
+                                ),
                               ),
-                              Image.network(
-                                "https://previews.123rf.com/images/winnond/winnond2008/winnond200800004/152889333-find-nearby-restaurants-that-open-for-food-delivery-or-search-location-of-the-restaurant-for-a-meal-.jpg",
-                                height: 200,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://www.emilyluxton.co.uk/wp-content/uploads/2017/06/abhishek-sanwa-limbu-782224-unsplash-800x534.jpg",
+                                  height: 200,
+                                ),
                               ),
-                              Image.network(
-                                "https://img.freepik.com/free-vector/modern-restaurant-menu-fast-food_52683-48982.jpg",
-                                height: 200,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://holidaynepaltreks.com/wp-content/uploads/2018/12/bhojan-griha.jpg",
+                                  height: 200,
+                                ),
                               ),
-                              Image.network(
-                                "https://previews.123rf.com/images/winnond/winnond2008/winnond200800004/152889333-find-nearby-restaurants-that-open-for-food-delivery-or-search-location-of-the-restaurant-for-a-meal-.jpg",
-                                height: 200,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://b.zmtcdn.com/data/pictures/3/18426303/9a859a16bb7e2259988c65fcee21babe.jpg",
+                                  height: 250,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://img.freepik.com/free-vector/modern-restaurant-menu-fast-food_52683-48982.jpg",
+                                  height: 180,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://c8.alamy.com/comp/2EENMYP/fast-food-menu-template-for-fastfood-restaurant-or-cafe-vector-sketch-price-list-for-hot-dog-and-fries-combo-pizza-or-cheeseburger-and-hamburger-san-2EENMYP.jpg",
+                                  height: 200,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://media-cdn.tripadvisor.com/media/photo-p/0e/42/c8/37/the-menu.jpg",
+                                  height: 300,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                                  height: 300,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://images.pexels.com/photos/541216/pexels-photo-541216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                                  height: 300,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  "https://img.freepik.com/free-vector/burgers-restaurant-menu-template_23-2149005028.jpg?w=2000",
+                                  height: 250,
+                                ),
                               ),
                             ],
                           ),
